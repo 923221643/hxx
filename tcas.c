82d3111091d397eb10de461c651fd76df101e33e
923221643 <923221643@qq.com>
2019-05-05 19:07:26 +0800

    v24

--- tcas.c
+++ tcas.c
@@ -87,7 +87,7 @@ bool Non_Crossing_Biased_Descend()
     int upward_crossing_situation;
     bool result;
 
-    upward_preferred = (Up_Separation + NOZCROSS) > Down_Separation;
+    upward_preferred = Up_Separation > Down_Separation;
     if (upward_preferred)
     {
 	result = Own_Below_Threat() && (Cur_Vertical_Sep >= MINSEP) && (Down_Separation >= ALIM());
